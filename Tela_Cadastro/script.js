5a4461652f8eac1bcdf0c49b4e33b7bace84ff02
Fernando Henrique Silva <fernandoagro.654@gmail.com>
2022-05-08 12:23:38 -0300

    criada a função para aparecer a msg de cadastro e de erro, aplicado funçao para localstorage

--- Tela_Cadastro/script.js
+++ Tela_Cadastro/script.js
@@ -17,15 +17,21 @@ let  confirmaSenha= document.querySelector('#confirmaSenha')
 let labelConfirmaSenha = document.querySelector('#labelConfirmaSenha')
 let validConfimaSenha = false
 
+let msgError = document.querySelector('#msgError')
+let msgSuccess = document.querySelector('#msgSuccess')
+
 nome.addEventListener('keyup', () => {
     if(nome.value.length <= 2) {
         labelNome.setAttribute('style', 'color:red')
         labelNome.innerHTML = 'Nome *Insira no minimo 3 caracteres'
         nome.setAttribute('style', 'border-color: red')
+        validNome = false
+
     }else {
         labelNome.setAttribute('style', 'color:green')
         labelNome.innerHTML = 'Nome'
         nome.setAttribute('style', 'border-color: green')
+        validNome = true
     }
 })
 
@@ -34,10 +40,12 @@ usuario.addEventListener('keyup', () => {
         labelUsuario.setAttribute('style', 'color:red')
         labelUsuario.innerHTML = 'Usuario *Insira no minimo 5 caracteres'
         usuario.setAttribute('style', 'border-color: red')
+        validUsuario = false
     }else {
         labelUsuario.setAttribute('style', 'color:green')
         labelUsuario.innerHTML = 'Usuario'
         usuario.setAttribute('style', 'border-color: green')
+        validUsuario = true
     }
 })
 
@@ -46,10 +54,12 @@ senha.addEventListener('keyup', () => {
         labelSenha.setAttribute('style', 'color:red')
         labelSenha.innerHTML = 'Senha *Insira no minimo 6 caracteres'
         senha.setAttribute('style', 'border-color: red')
+        validSenha= false
     }else {
         labelSenha.setAttribute('style', 'color:green')
         labelSenha.innerHTML = 'Senha'
         senha.setAttribute('style', 'border-color: green')
+        validSenha= true
     }
 })
 
@@ -58,19 +68,44 @@ confirmaSenha.addEventListener('keyup', () => {
         labelConfirmaSenha.setAttribute('style', 'color:red')
         labelConfirmaSenha.innerHTML = 'Confirmar Senha *As senhas não conferem'
         confirmaSenha.setAttribute('style', 'border-color: red')
+        validConfimaSenha = false
     }else {
         labelConfirmaSenha.setAttribute('style', 'color:green')
         labelConfirmaSenha.innerHTML = 'Confirmar Senha'
         confirmaSenha.setAttribute('style', 'border-color: green')
+        validConfimaSenha = true
     }
 })
 
 
 function cadastrar() {
     if(validNome || validUsuario || validSenha || validConfimaSenha){
+        let listaUser = JSON.parse(localStorage.getItem('listaUser') || '[]')
+
+        listaUser.push(
+            {
+                nomeCad: nome.value,
+                userCad: usuario.value,
+                senhaCad: senha.value
+            }
+        )
+
+        localStorage.setItem('listaUser', JSON.stringify(listaUser))
+
+        msgSuccess.setAttribute('style', 'display: block')
+        msgSuccess.innerHTML = '<strong>Cadastrando usuario...</strong>'
+        msgError.setAttribute('style', 'display: none') 
+        msgError.innerHTML = ''
+
+        setTimeout(() => {
+            window.location.href = 'http://127.0.0.1:5500/Tela_Login/index.html'
+        }, 30000)
 
     } else {
-        alert('Campos estão vazios')
+        msgError.setAttribute('style', 'display: block') 
+        msgError.innerHTML = '<strong>Preencha todos os campos corretamente antes de cadastrar.</strong>'
+        msgSuccess.setAttribute('style', 'display: none')
+        msgSuccess.innerHTML = ''
     }
 }
 
